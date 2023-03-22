92ee4dabdb980a3e17cb7d97dd98752155aa680a
Drake Wright <rdrakewright@gmail.com>
2023-03-22 08:16:14 -0500

    Initial Commit

---
+++ Text Adventure Retheme/Text_Adventure_RethemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_Adventure_RethemeApp.swift
+//  Text Adventure Retheme
+//
+//  Created by Drake Wright on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Text_Adventure_RethemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
